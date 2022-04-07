f7afb0f1fb062951f063371f042cbc5a88b4154f
Rizki Utama Fauzi <100134229+Rizukiuf@users.noreply.github.com>
2022-04-07 22:13:50 +0700

    Update Nomor 35 Program KelBil.cpp

--- Praktikum 6/Nomor 35 Program KelBil.cpp
+++ Praktikum 6/Nomor 35 Program KelBil.cpp
@@ -1,38 +1,55 @@
 /* Nama File 	: KelBil.cpp
    Deskripsi 	: Menentukan kelipatan bilangan terkecil jika diberikan sebuah array integer A sebarang
    Pembuat   	: Rizki Utama Fauzi - 24060121130050
-   Tgl Pembuatan: Rabu, 30-03-2022 (tanggal pembuatan c) - tanggal 03-04-2022 pindah ke cpp ini
+   Tgl Pembuatan: Minggu, 03-04-2022 - ralat Kamis, 07-04-2022
 */
 
-#include<stdio.h>
 
-int main(){
+#include<stdio.h>
+#include<stdbool.h>
+
+int main() {
 
     //kamus
-    int N, i, j, KPK;
+    int A[10], N, KPK, i, count;
+    bool next;
 
     //algoritma
     printf("Program menentukan kelipatan bilangan terkecil jika diberikan sebuah array integer A sebarang \n");
-    printf("Masukan untuk bilangan integer N (N>0): ");
+    printf("Masukan untuk jumlah angka: ");
     scanf("%d", &N);
-    if (N <= 0) {
-        printf("N harus positif");
+    printf("Masukan kumpulan sebanyak %d angka: ", N);
+    for (i = 1; i <= N; i++) {
+        scanf("%d", &A[i]);
     }
-    else {  //N>0
-        int A[N];
-        printf("Kumpulan angka (urut ke atas) = ");
-        for (i = 1; i <= N; i++) {
-            scanf("%d", &A[i]);
+    KPK = 0;
+    for (i = 1; i <= N; i++) {
+        if (KPK < A[i]) {
+            KPK = A[i];
         }
-        KPK = 1;    //inisialisasi
-        for (j = 1; j <= N; j++) {
-            if (A[j] % KPK == 0) {
-                KPK = A[j];
-            } else {
-                KPK *= A[j];
+    }
+    next = true;
+    while (next == true) {
+        count = 0;
+        for (i = 1; i <= N; i++) {
+            if (KPK % A[i] == 0) {
+                count++;
             }
         }
-        printf("\nKelipatan terkecilnya adalah %d\n", KPK);
+        //Keadaan stop rekursif
+        if (count == N) {
+            next = false;
+        }
+        //Meneruskan rekursif
+        else {
+            KPK++;
+        }
     }
+    //Menampilkan hasil
+    printf("\nKelipatan bilangan terkecil dari (");
+    for(i = 1; i < N; i++) {
+        printf("%d,", A[i]);
+    }
+    printf("%d) = %d\n", A[i], KPK);
     return 0;
 }
